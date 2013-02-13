c4aec6348e86ca661a36f3904d94e12060e5f4c9
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-13 15:02:56 +1100

    Adding the Submit JavaScript

---
+++ gs/content/js/submit/browser/javascript/submit.js
@@ -0,0 +1,43 @@
+jQuery.noConflict();
+var GSDisableSubmit = function () {
+
+    // Private variables
+    // The disabled  button that says "Processing", followed by
+    //    ellipsis.
+    var newButtonText = '<button class="processing btn" disabled="disabled">Processing\u2026</button>';
+    
+    // Private methods
+    var handle_submit = function(event) {
+        var form = null;
+        var buttons = null;
+        form = jQuery(this);
+        buttons = form.find(':submit');
+        buttons.each(function(){disable_button(this);});
+        return true;
+    }
+    var disable_button = function(submitButton) {
+        // --=mpj17=-- If I disable the button the form is not 
+        //      processed by zope.formlib, as disabled buttons are
+        //      "unsuccessful" by definition, so they are not POSTed
+        //      to Zope as part of the form data. So, I *hide* the
+        //      button (using CSS) and add a disabled button in its
+        //      place.
+        var s = null;
+        var newButton = null;
+        s = jQuery(submitButton);
+        s.addClass('hiddenType').css('display', 'none');
+
+        newButton = s.before(newButtonText);
+        
+        return true;
+    }
+    // Public methods and properties
+    return {
+        init: function ( ) {        
+            jQuery('form').submit(handle_submit);
+        }
+    };
+}(); // GSDisableSubmit
+
+jQuery(window).load(GSDisableSubmit.init);
+
