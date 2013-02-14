c2ac91961e8f96b0ca7e0c2faadf6662971a2fc2
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-14 12:25:44 +1100

    Fixing the JS so there is only one variable per declaration scope.

--- gs/content/js/submit/browser/javascript/submit.js
+++ gs/content/js/submit/browser/javascript/submit.js
@@ -4,26 +4,25 @@ var GSDisableSubmit = function () {
     // Private variables
     // The disabled  button that says "Processing", followed by
     //    ellipsis.
-    var newButtonText = '<button class="processing btn" disabled="disabled">Processing\u2026</button>';
+    var newButtonText = '<button class="processing btn" '+
+                          'disabled="disabled">Processing\u2026</button>';
     
     // Private methods
-    var handle_submit = function(event) {
-        var form = null;
-        var buttons = null;
+    function handle_submit(event) {
+        var form = null, buttons = null;
         form = jQuery(this);
         buttons = form.find(':submit');
         buttons.each(function(){disable_button(this);});
         return true;
     }
-    var disable_button = function(submitButton) {
+    function disable_button(submitButton) {
         // --=mpj17=-- If I disable the button the form is not 
         //      processed by zope.formlib, as disabled buttons are
         //      "unsuccessful" by definition, so they are not POSTed
         //      to Zope as part of the form data. So, I *hide* the
         //      button (using CSS) and add a disabled button in its
         //      place.
-        var s = null;
-        var newButton = null;
+        var s = null, newButton = null;
         s = jQuery(submitButton);
         s.addClass('hiddenType').css('display', 'none');
 
@@ -40,4 +39,3 @@ var GSDisableSubmit = function () {
 }(); // GSDisableSubmit
 
 jQuery(window).load(GSDisableSubmit.init);
-
