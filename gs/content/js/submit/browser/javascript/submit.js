7d9bcb52a8e4ec7e8c1601170a92146109f22e45
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-13 11:19:11 -0700

    Minor tweeks to try and track down an IE8 issue.

--- gs/content/js/submit/browser/javascript/submit.js
+++ gs/content/js/submit/browser/javascript/submit.js
@@ -1,6 +1,16 @@
+// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). http://groupserver.org/downloads/license/
+//
+// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+// WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+// FITNESS FOR A PARTICULAR PURPOSE.
 jQuery.noConflict();
-var GSDisableSubmit = function () {
 
+function GSDisableSubmit() {
     // Private variables
     // The disabled  button that says "Processing", followed by
     //    ellipsis.
@@ -9,12 +19,15 @@ var GSDisableSubmit = function () {
     
     // Private methods
     function handle_submit(event) {
-        var form = null, buttons = null;
+        var form=null, buttons=null;
+
         form = jQuery(this);
         buttons = form.find(':submit');
         buttons.each(function(){disable_button(this);});
+
         return true;
-    }
+    } // handle_submit
+
     function disable_button(submitButton) {
         // --=mpj17=-- If I disable the button the form is not 
         //      processed by zope.formlib, as disabled buttons are
@@ -22,18 +35,19 @@ var GSDisableSubmit = function () {
         //      to Zope as part of the form data. So, I *hide* the
         //      button (using CSS) and add a disabled button in its
         //      place.
-        var s = null, newButton = null;
+        var s=null, newButton=null;
+
         s = jQuery(submitButton);
         s.addClass('hiddenType').css('display', 'none');
-
         newButton = s.before(newButtonText);
-        
+
         return true;
-    }
+    } // disable_button
+
     // Public methods and properties
     return {
         init: function ( ) {
-            var buttons = null;
+            var buttons=null;
             jQuery('form').submit(handle_submit);
             buttons = jQuery('form input[type=submit]');
             if ((buttons !== null) 
@@ -44,6 +58,10 @@ var GSDisableSubmit = function () {
             }
         }
     };
-}(); // GSDisableSubmit
+} //GSDisableSubmit
 
-jQuery(window).load(GSDisableSubmit.init);
+jQuery(window).load(function () {
+    var s=null;
+    s = GSDisableSubmit();
+    s.init()
+});
