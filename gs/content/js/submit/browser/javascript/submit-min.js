2580e419763f180b37e531e919881468a2715bdd
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-13 11:19:31 -0700

    Upping the version number and adding a Copyright block

--- gs/content/js/submit/browser/javascript/submit-min.js
+++ gs/content/js/submit/browser/javascript/submit-min.js
@@ -1,6 +1,6 @@
-jQuery.noConflict();var GSDisableSubmit=function(){var b='<button class="processing btn" disabled="disabled">Processing\u2026</button>';
+jQuery.noConflict();function GSDisableSubmit(){var b='<button class="processing btn" disabled="disabled">Processing\u2026</button>';
 function c(f){var e=null,d=null;e=jQuery(this);d=e.find(":submit");d.each(function(){a(this)
 });return true}function a(d){var f=null,e=null;f=jQuery(d);f.addClass("hiddenType").css("display","none");
 e=f.before(b);return true}return{init:function(){var d=null;jQuery("form").submit(c);
 d=jQuery("form input[type=submit]");if((d!==null)&&(typeof d!=="undefined")&&(typeof d.button!=="undefined")){d.button().addClass("btn")
-}}}}();jQuery(window).load(GSDisableSubmit.init);
+}}}}jQuery(window).load(function(){var a=null;a=GSDisableSubmit();a.init()});
