58d1e0dbe3fc88a7e3ab589c5956a92cbfb8d9ed
Michael JasonSmith <mpj17@onlinegroups.net>
2016-01-25 10:16:02 -0800

    Minifying

--- gs/content/js/submit/browser/javascript/submit-min.js
+++ gs/content/js/submit/browser/javascript/submit-min.js
@@ -1,6 +1,3 @@
-"use strict";jQuery.noConflict();function GSDisableSubmit(){var b='<button class="processing btn" disabled="disabled">Processing\u2026</button>';
-function c(f){var e=null,d=null;e=jQuery(this);d=e.find(":submit");d.each(function(){a(this)
-});return true}function a(d){var f=null,e=null;f=jQuery(d);f.addClass("hiddenType").css("display","none");
-e=f.before(b);return true}return{init:function(){var d=null;jQuery("form").submit(c);
-d=jQuery("form input[type=submit]");if((d!==null)&&(typeof d!=="undefined")&&(typeof d.button!=="undefined")){d.button().addClass("btn")
-}}}}jQuery(window).load(function(){var a=null;a=GSDisableSubmit();a.init()});
+'use strict';jQuery.noConflict();
+function GSDisableSubmit(){function b(a){a=a=null;a=jQuery(this);a=a.find(":submit");a.each(function(){var a=null,a=jQuery(this);a.addClass("hiddenType").css("display","none");a.before(c)});return!0}var c='<button class="processing btn" disabled="disabled">Processing…</button>';return{init:function(){var a=null;jQuery("form").submit(b);a=jQuery("form input[type=submit]");null!==a&&("undefined"!==typeof a&&"undefined"!==typeof a.button)&&a.button().addClass("btn")}}}
+jQuery(window).load(function(){var b=null,b=GSDisableSubmit();b.init()});
